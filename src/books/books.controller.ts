65d6d528fa04944f5ed6b87432d2aba9b88bbf8c
Ed <edward.syasin@gmail.com>
2022-05-16 01:28:31 +0300

    Добавить контроллер и сервис для книг

---
+++ src/books/books.controller.ts
@@ -0,0 +1,36 @@
+import {Body, Controller, Delete, Get, Inject, Param, Patch, Post} from '@nestjs/common';
+import {BooksService} from "./books.service";
+import CreateBookDto from "./dto/create-book.dto";
+import UpdateBookDto from "./dto/update-book.dto";
+
+@Controller('api/books')
+export class BooksController {
+
+	constructor(private booksService: BooksService) {}
+
+	@Get('/:id')
+	findById(@Param('id') id: string) {
+		return this.booksService.findById(id);
+	}
+
+	@Get()
+	findAll() {
+		return this.booksService.findAll();
+	}
+
+	@Post()
+	create(@Body() dto: CreateBookDto) {
+		return this.booksService.addBook(dto);
+	}
+
+	@Delete('/:id')
+	deleteBook(@Param('id') id: string) {
+		this.booksService.deleteBook(id);
+		return { message: 'OK' }
+	}
+
+	@Patch('/:id')
+	updateBook(@Param('id') id: string, @Body() dto: UpdateBookDto) {
+		return this.booksService.updateBook(id, dto);
+	}
+}
