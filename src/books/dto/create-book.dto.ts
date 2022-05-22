4f1544c290aa5085c527406386afec69a05045e0
Ed <edward.syasin@gmail.com>
2022-05-22 20:30:03 +0300

    Добавить пайп для валидации ДТОшек

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty } from "class-validator";
+
 export default class CreateBookDto {
+	@IsNotEmpty()
 	readonly title: string
+
 	readonly description?: string
+
+	@IsNotEmpty()
 	readonly author: string
 }
